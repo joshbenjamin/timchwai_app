6b2acc59bf11fedd2f536a40df639ad9aaab9a95
Josh Benjamin <joshbenjamin13@gmail.com>
2023-04-21 14:15:53 +0200

    Correct backend URL

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
 
 const axiosInstance = axios.create({
-  baseURL: `http://${REACT_APP_API_URL}`, // Replace with your backend base URL
+  baseURL: `${REACT_APP_API_URL}`, // Replace with your backend base URL
   timeout: 5000, // Optional: set a request timeout in milliseconds
   headers: {
     'Content-Type': 'application/json',
