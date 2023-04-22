4ad88c3c5f48b190d0c9e3b9d9dcd50afaaa1b17
Josh Benjamin <joshbenjamin13@gmail.com>
2023-04-22 20:44:06 +0200

    Default is home

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './screens/Home';
-import PlayerScreen from './screens/PlayerScreen';
 import TIMCHWAISCREEN from './screens/TIMCHWAIScreen';
+import PlayerScreen from './screens/PlayerScreen';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route index path="/" element={<Home />} />
+          <Route index path="/" element={<TIMCHWAISCREEN />} />
           <Route path="/player/:playerId" element={<PlayerScreen />} />
-          <Route path="/timchwai/" element={<TIMCHWAISCREEN />} />
-          {/* <Route path='/timchwai' */}
         </Routes>
       </Router>
     </div>
