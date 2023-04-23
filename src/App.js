acb802306a4e84030f625025daeb60350d19af56
Josh Benjamin <joshbenjamin13@gmail.com>
2023-04-23 19:18:21 +0200

    Added Analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 import TIMCHWAISCREEN from './screens/TIMCHWAIScreen';
 import PlayerScreen from './screens/PlayerScreen';
@@ -8,6 +9,7 @@ import PlayerScreen from './screens/PlayerScreen';
 function App() {
   return (
     <div className="App">
+      <Analytics />
       <Router>
         <Routes>
           <Route index path="/" element={<TIMCHWAISCREEN />} />
