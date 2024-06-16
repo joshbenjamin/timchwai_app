ab09de16924202de537bb7d32122e25b99de58eb
Joshua Benjamin <joshbenjamin13@gmail.com>
2024-06-16 16:31:10 +0200

    Added url routes for 2024 international tournaments

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 
+import Euro2024Screen from './screens/Euro2024Screen';
+import CopAm2024Screen from './screens/CopAm2024Screen';
 import TIMCHWAISCREEN from './screens/TIMCHWAIScreen';
 import PlayerScreen from './screens/PlayerScreen';
 import RandomScreen from './screens/RandomScreen';
@@ -14,6 +16,8 @@ function App() {
       <Router>
         <Routes>
           <Route index path="/" element={<RandomScreen />} />
+          <Route index path="/Euro2024" element={<Euro2024Screen />} />
+          <Route index path="/CopaAmerica2024" element={<CopAm2024Screen />} />
           <Route index path="/filter" element={<TIMCHWAISCREEN />} />
           <Route path="/player/:playerId" element={<PlayerScreen />} />
         </Routes>
