11ecbe7f262c5e5808a73368a2948d6278dec5aa
Joshua Benjamin <joshbenjamin13@gmail.com>
2024-06-17 17:56:03 +0200

    Added Euro2024Screen

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 
-import Euro2024Screen from './screens/Euro2024Screen';
-import CopAm2024Screen from './screens/CopAm2024Screen';
 import TIMCHWAISCREEN from './screens/TIMCHWAIScreen';
 import PlayerScreen from './screens/PlayerScreen';
 import RandomScreen from './screens/RandomScreen';
+import Euro2024Screen from './screens/Euro2024Screen';
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
         <Routes>
           <Route index path="/" element={<RandomScreen />} />
           <Route index path="/Euro2024" element={<Euro2024Screen />} />
-          <Route index path="/CopaAmerica2024" element={<CopAm2024Screen />} />
           <Route index path="/filter" element={<TIMCHWAISCREEN />} />
           <Route path="/player/:playerId" element={<PlayerScreen />} />
         </Routes>
